9fc1b58d438a68e978f458863ca63ba2a08639a0
Brendo Eric <brendoerick800@outlook.com>
2025-03-18 09:56:06 -0300

    Atts

--- ResponsabilidadesClasse/Servicos/ProdutoServico.cs
+++ ResponsabilidadesClasse/Servicos/ProdutoServico.cs
@@ -30,27 +30,44 @@ namespace ResponsabilidadesClasse.Servicos
                 var resposta = Console.ReadLine();
                 Console.Clear();
 
-                switch (resposta)
+                try
                 {
-                    case "1":
-                        Listar();
-                        break;
-                    case "2":
-                        Cadastrar();
-                        break;
-                    case "3":
-                        Atualizar();
-                        break;
-                    case "4":
-                        Remover();
-                        break;
-                    case "5":
-                        Desativar();
-                        break;
+                    switch (resposta)
+                    {
+                        case "1":
+                            Listar();
+                            break;
+                        case "2":
+                            Cadastrar();
+                            break;
+                        case "3":
+                            Atualizar();
+                            break;
+                        case "4":
+                            Remover();
+                            break;
+                        case "5":
+                            Desativar();
+                            break;
 
-                    default:
-                        Console.WriteLine("Selecione uma opcao valida");
-                        break;
+                        default:
+                            Console.WriteLine("Selecione uma opcao valida");
+                            continue;
+                    }
+
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Pressione uma tecla para continuar!");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ocorreu um erro fatal no programa, veja a mensagem do erro e contate o suporte:" ex.Message);
+                    Console.WriteLine("Pressione uma tecla para continuar!");
+                    Console.ReadKey();
                 }
             }
         }
@@ -58,13 +75,9 @@ namespace ResponsabilidadesClasse.Servicos
         private void Cadastrar()
         {
             var produto = ColetarDadosProduto();
-            if (produto is null)
-                return;
-
             _repositorio.Inserir(produto);
-            Console.WriteLine($"{produto.Nome} cadastrado com sucesso!");
-            Console.WriteLine($"Aperte uma tecla para prosseguir!");
-            Console.ReadKey();
+
+            throw new InvalidOperationException($"{produto.Nome} cadastrado com sucesso!");
         }
         private void Listar()
         {
@@ -80,8 +93,7 @@ namespace ResponsabilidadesClasse.Servicos
             {
                 Console.WriteLine($"Identificador => {p.IdentificadorProduto};Nome => {p.Nome};Valor => {p.Valor};{(p.Situacao ? "Ativo" : "Inativo")}");
             }
-
-            Console.WriteLine("Para sair da listagem apertar uma tecla!");
+            Console.WriteLine("Para sair da listagem aperte uma tecla!");
             Console.ReadKey();
             //produtos.ForEach(p => Console.WriteLine($"Identificar => {p.IdentificadorProduto};Nome => {p.Nome};Valor => {p.Valor};{(p.Situacao ? "Ativo" : "")}"))
 
@@ -92,44 +104,34 @@ namespace ResponsabilidadesClasse.Servicos
         private void Atualizar()
         {
             var identificador = PerguntarIdentificador("Atualizar");
-            if (!identificador.HasValue)
-                return;
-
-            _repositorio.Desativar(identificador.Value);
+            
             var produto = ColetarDadosProduto();
-            if (produto is null)
-                return;
-
-            produto.IdentificadorProduto = identificador.Value;
-
+            produto.IdentificadorProduto = (identificador);
             _repositorio.Atualizar(produto);
+
+            throw new InvalidOperationException($"{produto.Nome} atualizado com sucesso para o identificador {identificador}!");
+
         }
         private void Remover()
         {
             var identificador = PerguntarIdentificador("remover");
-            if (!identificador.HasValue)
-                return;
-            _repositorio.Remover(identificador.Value);
+                _repositorio.Remover(identificador);
         }
         private void Desativar()
         {
             var identificador = PerguntarIdentificador("desativar");
-            if (!identificador.HasValue)
-                return;
 
-            _repositorio.Desativar(identificador.Value);
+            _repositorio.Desativar(identificador);
         }
 
-        private Produto? ColetarDadosProduto()
+        private Produto ColetarDadosProduto()
         {
             Console.WriteLine("Qual o nome do produto que voce quer cadastrar?");
             var nomeProduto = Console.ReadLine();
 
             if (!Utilitarios.Validacoes.ValidarTamanhoTexto(nomeProduto, 3, 80))
             {
-                Console.WriteLine("O nome do produto deve conter de 3 a 80 caracteres");
-                Console.ReadKey();
-                return null;
+                throw new InvalidOperationException("O nome do produto deve conter de 3 a 80 caracteres");
             }
             
             Console.WriteLine($"Qual o valor do produto {nomeProduto}?");
@@ -137,9 +139,7 @@ namespace ResponsabilidadesClasse.Servicos
 
             if (!Utilitarios.Validacoes.ValidarSeNumeroDecimalBrasileiro(valorString))
             {
-                Console.WriteLine("O valor deve ser informado no seguinte formato 0,00");
-                Console.ReadKey();
-                return null;
+                throw new InvalidOperationException("O valor deve ser informado no seguinte formato 0,00");
             }
 
             var valor = Convert.ToDecimal(valorString);
@@ -152,32 +152,22 @@ namespace ResponsabilidadesClasse.Servicos
             });
 
         }
-        private int? PerguntarIdentificador(string nomeAcao)
+        private int PerguntarIdentificador(string nomeAcao)
         {
             
-            Console.WriteLine($"Por favor, Informe o nome do produto para {nomeAcao}?");
+            Console.WriteLine($"Por favor, Informe o nome do produto para {nomeAcao}:");
             string identificadorInfomardoString = Console.ReadLine();
 
             if (!int.TryParse(identificadorInfomardoString, out _))
-            {
-                Console.WriteLine("O identificador informado nao é valido.");
-                Console.ReadKey();
-                return null;
-            }
+                throw new InvalidOperationException("o identificador nao é valido");
 
             var identificadorInformado = Convert.ToInt32(identificadorInfomardoString);
 
             if (!_repositorio.SeExiste(identificadorInformado))
+            throw new InvalidOperationException("Este produto nao existe, tente novamente");
 
-            {
-                Console.WriteLine("Este produto nao existe, tente novamente.");
-                Console.ReadKey();
-                return null;
-            }
-            else
-            {
                return identificadorInformado;
-            }
+            
         }
 
 
